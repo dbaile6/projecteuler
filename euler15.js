aacff535d457d5f170024b4f8b09f22d5ec56afa
Dylan Bailey <dylanba1@gmail.com>
2018-04-09 00:34:00 -0400

    Refactor to ES6 and comment code

--- euler15.js
+++ euler15.js
@@ -1,14 +1,27 @@
-var route = [];
+// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
-var getRoute = function (sizeX, sizeY) {
+
+// How many such routes are there through a 20×20 grid?
+
+
+//Initializes route path
+
+// 1. Create arrays out of inputs
+// 2.Create a fail state
+// 3. Creates an iterative formula to find out routes.
+
+let route = [];
+
+let getRoute = (sizeX, sizeY) => {
+    //Turns sizeX into an array
     route[sizeX] = route[sizeX] || []
     if (route[sizeX][sizeY]) {
         return route[sizeX][sizeY];
     }
-
+    //If there is no grid return 1
     if (sizeX == 0 || sizeY == 0) return 1;
-    //Time to move?
-    var routes = getRoute(sizeX - 1, sizeY);
+    //Figures out if you can move
+    let routes = getRoute(sizeX - 1, sizeY);
     routes += getRoute(sizeX, sizeY - 1);
     route[sizeX][sizeY] = routes;
     return routes;
@@ -16,3 +29,4 @@ var getRoute = function (sizeX, sizeY) {
 
 console.log(getRoute(2, 2));
 console.log(getRoute(20, 20));
+

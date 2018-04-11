e169f17394703e697e8132f5440eae04b8cee7ba
Dylan Bailey <dylanba1@gmail.com>
2018-04-10 23:32:26 -0400

    Add Python Folder

---
+++ python/euler1.py
@@ -0,0 +1,6 @@
+# Fairly self explanatory, especially in comparison to the Node/JS versions. Python syntax is incredibly simple and self-commenting.
+
+def multiple_3_and_5():
+    for number in range(1000):
+        if not number % 3 or not number % 5
+            yield number
